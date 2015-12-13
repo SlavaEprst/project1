05a5fe2f903e511b1d7cb6e35c593e3b194fded7
Slava <kossh@yandex.ru>
2015-12-14 01:13:13 +0300

    lesson 3 добавлен класс point

--- Program.cs
+++ Program.cs
@@ -10,23 +10,22 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sim1 = '*';
-            int x2 = 3;
-            int y2 = 5;
-            char sim2 = '#';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sim = '*';
+            p1.Draw();
 
-            Draw(x1, y1, sim1);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sim = '#';
+            p2.Draw();
+
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sim)
-        {
-
-Console.SetCursorPosition(x, y);
-           Console.Write(sim);
-            }
+        
 
     }
 }
