aba76bbf03b95e3ff0770101cf3bf98bf6dd079a
Slava <kossh@yandex.ru>
2015-12-14 00:44:10 +0300

    lesson 2 is ending

--- Program.cs
+++ Program.cs
@@ -10,8 +10,23 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("hello world!");
-            Console.Read();
+            int x1 = 1;
+            int y1 = 3;
+            char sim1 = '*';
+            int x2 = 3;
+            int y2 = 5;
+            char sim2 = '#';
+
+            Draw(x1, y1, sim1);
+            Console.ReadLine();
         }
+
+        static void Draw(int x, int y, char sim)
+        {
+
+Console.SetCursorPosition(x, y);
+           Console.Write(sim);
+            }
+
     }
 }
