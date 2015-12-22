fd1c083312f5f61d1925f3039600963969fd3400
Slava <kossh@yandex.ru>
2015-12-22 13:43:49 +0300

    удилил лишний код прримеров списков

--- Program.cs
+++ Program.cs
@@ -10,38 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(3, 8,'&');
-            p1.Draw();
 
-            Point p2 = new Point(1, 1,'#');
-            p2.Draw();
-
-            Point p3 = new Point(2, 7, '$');
-            p3.Draw();
-
-            Point p4 = new Point(5, 5, '^');
-            p4.Draw();
-
-            List<char> Sim = new List<char>();
-            Sim.Add('@');
-            Sim.Add('#');
-            Sim.Add('$');
-            Sim.Add('%');
-            Sim.Add('&');
-
-            foreach (char F in Sim)
-            {
-                Console.WriteLine(F);
-            }
-
-
-            List<Point> PointList = new List<Point>();
-            PointList.Add(p1);
-            PointList.Add(p2);
-            PointList.Add(p3);
-            PointList.Add(p4);
-
-            HorizontLine line = new HorizontLine(5, 20, 5,20, '#');
+            HorizontLine line = new HorizontLine(1, 20, 1,20, '+');
             line.Drow();
 
             Console.ReadLine();
