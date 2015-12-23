24de7a7d00843b4862ceaa6d5c71fdbbb00b570f
Slava <kossh@yandex.ru>
2015-12-24 00:23:27 +0300

    закомичены новые файлы - дирекшн фигура и змейка

---
+++ snake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class snake:figure
+    {
+        public snake(Point teil, int light, direction direction)
+        {
+            pList = new List<Point>();
+
+            for (int i = 0; i < light; i++)
+            {
+                Point p = new Point(teil);
+                p.Move(i, direction);
+                pList.Add(p);
+
+            }
+        }
+
+
+    }
+}
